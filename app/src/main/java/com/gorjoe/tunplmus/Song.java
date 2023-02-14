a9a5397c08897be1be5f2722dec77d2c10d99c4b
gorjoe <joeminecraft1234funs35@gmail.com>
2023-02-15 01:49:31 +0800

    SongList getting

---
+++ app/src/main/java/com/gorjoe/tunplmus/Song.java
@@ -0,0 +1,27 @@
+package com.gorjoe.tunplmus;
+
+public class Song {
+
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist) {
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){
+        return id;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getArtist(){
+        return artist;
+    }
+
+}
